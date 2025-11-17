a83483815ce37485dab722902e29e91a819f027d
arlen <349871234.o@gmail.com>
2025-11-17 11:47:18 -0500

    stupid minecraft things

--- src/teavm/java/net/lax1dude/eaglercraft/internal/teavm/ClientMain.java
+++ src/teavm/java/net/lax1dude/eaglercraft/internal/teavm/ClientMain.java
@@ -211,7 +211,7 @@ public class ClientMain {
 			systemOut.println("ClientMain: [INFO] launching eaglercraftx main thread");
 
 			try {
-				new MinecraftApplet().start();
+				new MinecraftApplet().run();
 			}catch(ContextLostError ex) {
 				systemErr.println("ClientMain: [ERROR] webgl context lost!");
 				try {
